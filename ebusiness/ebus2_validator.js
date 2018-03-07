930932fbab89fe9e5689e295c0a6b65a4a0c08e9
Conor Hegarty <117426336@umail.ucc.ie>
2018-03-07 13:24:08 +0000

    fix

--- ebusiness/ebus2_validator.js
+++ ebusiness/ebus2_validator.js
@@ -2,8 +2,12 @@
 function validateDetails(){
     
     var pin;
+    var name;
+    var email;
     
     pin= document.getElementById("user_pin").value;
+    name=document.getElementById("name").value;
+    email=document.getElementById("email").value;
     
     if (pin == ""){
         alert("Please enter your PIN");
@@ -11,7 +15,13 @@ function validateDetails(){
     else if (String(pin).length < 4){
         alert ("Please make sure you PIN is accurate");
     }
-    else{
+    else if(name == ""){
+        alert("Please enter your name");
+    }
+    else if(email == ""){
+        alert("Enter your email");
+    }
+        else{
         enablebtnPurchase();
     }
     
